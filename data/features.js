560732b9096d8f596eca7ab72587c7059dce2c52
shrungadithyaan <shrungadithyaan@gmail.com>
2025-06-18 17:44:52 +0530

    first files commit

---
+++ data/features.js
@@ -0,0 +1,32 @@
+import { BrainCircuit, Briefcase, LineChart, ScrollText , Headphones} from "lucide-react";
+
+export const features = [
+  {
+    icon: <BrainCircuit className="w-10 h-10 mb-4 text-primary" />,
+    title: "AI-Powered Career Guidance",
+    description:
+      "Get personalized career advice and insights powered by advanced AI technology.",
+  },
+  {
+    icon: <Briefcase className="w-10 h-10 mb-4 text-primary" />,
+    title: "Interview Preparation",
+    description:
+      "Practice with role-specific questions and get instant feedback to improve your performance.",
+  },
+  {
+    icon: <LineChart className="w-10 h-10 mb-4 text-primary" />,
+    title: "Industry Insights",
+    description:
+      "Stay ahead with real-time industry trends, salary data, and market analysis.",
+  },
+  {
+    icon: <ScrollText className="w-10 h-10 mb-4 text-primary" />,
+    title: "Smart Resume Creation",
+    description: "Generate ATS-optimized resumes with AI assistance.",
+  },
+  {
+    icon: <Headphones className="w-10 h-10 mb-4 text-primary" />,
+    title: "AI Virtual Interview Coach",
+    description: "AI based Virtual interview , just real-world skills. Let's level up your portfolio and your interview game! .",
+  },
+];
